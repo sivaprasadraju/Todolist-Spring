7704da73a1a28daafcf5620a32636c9dd6f60668
sivaprasadraju <sraju728@gmail.com>
2017-10-11 14:39:05 +0530

    Change name of the project

--- src/main/java/com/example/todolistspring/TodolistSpringApplication.java
+++ src/main/java/com/example/todolistspring/TodolistSpringApplication.java
@@ -1,12 +1,12 @@
-package com.example.demo;
+package com.example.todolistspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TodolistSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TodolistSpringApplication.class, args);
 	}
 }
